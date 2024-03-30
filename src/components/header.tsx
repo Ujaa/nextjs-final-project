f7cfded474e6b87cf66e8d384b28c1225b09eb31
Yujin <yujinyll9148@gmail.com>
2024-03-30 23:17:31 +0900

    feat: header 추가
    - 경로 별칭 설정
    - 폴더 구조화

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <h1>The New York Times Best Seller Explorer</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
